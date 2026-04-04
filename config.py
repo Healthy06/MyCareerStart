42cb868c7a4a2b9fd56e06e6f867a84b3c43097c
kydex <104749015+kyde0x@users.noreply.github.com>
2026-04-05 02:42:39 +0700

    .

--- config.py
+++ config.py
@@ -3,9 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-HH_API_URL = "https://api.hh.ru/vacancies"
-HH_API_REGION = 3
-HH_API_VACANCIES_PER_PAGE = 5  
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions"
+MODEL_NAME = "deepseek/deepseek-chat"
+
+HH_API_URL = "https://api.hh.ru/vacancies"
+HH_API_REGION = 1
+HH_API_VACANCIES_PER_PAGE = 10
+
+DB_PATH = os.getenv("DB_PATH", "career_bot.db")
